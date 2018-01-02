a185a3b3be063ac52f0ea09bc4d1b54c8dcb83f8
Capizzi Filippo <capizzi@rsaweb.com>
2018-01-02 18:07:05 +0100

    Aggiunta possibilità visualizzazione dettaglio scadenze per cliente

---
+++ Facile/ScadenzeDetails.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Facile.Interfaces;
+using Facile.Models;
+using Facile.ViewModels;
+using SQLite;
+using Syncfusion.Data;
+using Syncfusion.SfDataGrid.XForms;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Facile
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ScadenzeDetails : ContentPage
+	{
+		private readonly ScadenzeInfo info_;
+		private readonly SQLiteAsyncConnection dbcon_;
+
+		public ScadenzeDetails(ScadenzeInfo info)
+		{
+			info_ = info;
+			InitializeComponent();
+			dbcon_ = DependencyService.Get<ISQLiteDb>().GetConnection();
+			Title = info.CliDesc;
+
+			GridTableSummaryRow summaryRow1 = new GridTableSummaryRow();
+			summaryRow1.Title = "Totale {Totale} - Num. : {ScaCount}";
+			summaryRow1.ShowSummaryInRow = true;
+			summaryRow1.Position = Position.Bottom;
+			summaryRow1.SummaryColumns.Add(new GridSummaryColumn()
+			{
+				Name = "Totale",
+				MappingName = "sca_importo",
+				Format = "{Sum:c}",
+				SummaryType = SummaryType.DoubleAggregate
+			});
+			summaryRow1.SummaryColumns.Add(new GridSummaryColumn()
+			{
+				Name = "ScaCount",
+				MappingName = "sca_num",
+				Format = "{Count:#,#}",
+				SummaryType = SummaryType.CountAggregate
+			});
+			dataGrid.TableSummaryRows.Add(summaryRow1);
+		}
+
+		protected async override void OnAppearing()
+		{
+			dataGrid.IsBusy = true;
+			string sql = String.Format("SELECT * FROM scadenze WHERE sca_relaz = 0 AND sca_pagato = 0 AND sca_cli_for = {0} ORDER BY sca_data, sca_num", info_.CliId);
+			var scaList = await dbcon_.QueryAsync<Scadenze>(sql);
+			var scaCollection = new ObservableCollection<Scadenze>(scaList);
+			dataGrid.ItemsSource = scaCollection;
+			dataGrid.IsBusy = false;
+			base.OnAppearing();
+		}
+
+		protected override void OnSizeAllocated(double width, double height)
+		{
+			base.OnSizeAllocated(width, height);
+			if (Device.Idiom == TargetIdiom.Phone)
+			{
+				if (width > height)
+				{
+					foreach (var col in dataGrid.Columns)
+					{
+						if (col.HeaderText == "Descrizione") col.IsHidden = false;
+					}
+				}
+				else
+				{
+					foreach (var col in dataGrid.Columns)
+					{
+						if (col.HeaderText == "Descrizione") col.IsHidden = true;
+					}
+				}
+			}
+		}
+
+	}
+}
--- Facile/ScadenzeElenco.xaml.cs
+++ Facile/ScadenzeElenco.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using Facile.Extension;
 using Facile.Interfaces;
+using Facile.Models;
 using Facile.ViewModels;
 using SQLite;
 using Syncfusion.Data;
@@ -16,13 +17,20 @@ namespace Facile
 	public partial class ScadenzeElenco : ContentPage
 	{
 		private readonly SQLiteAsyncConnection dbcon_;
+		private int swipeIndex_;
+		private ScadenzeInfo swipeData_;
+		private Image leftImage;
+		private bool first;
 
 		public ScadenzeElenco()
 		{
 			InitializeComponent();
+			swipeIndex_ = 0;
+			swipeData_ = null;
+			leftImage = null;
+			first = true;
 			dbcon_ = DependencyService.Get<ISQLiteDb>().GetConnection();
 			dataGrid.ColumnSizer = Syncfusion.SfDataGrid.XForms.ColumnSizer.LastColumnFill;
-
 
 			GridTableSummaryRow summaryRow1 = new GridTableSummaryRow();
 			summaryRow1.Title = "Totale {Totale} - Numero Cli. : {CliCount}";
@@ -43,23 +51,32 @@ namespace Facile
 				SummaryType = SummaryType.CountAggregate
 			});
 			dataGrid.TableSummaryRows.Add(summaryRow1);
+
+			if (Device.Idiom == TargetIdiom.Phone && Device.RuntimePlatform == Device.Android)
+			{
+				searchBar.HeightRequest = 25;
+			}
 		}
 
 		protected async override void OnAppearing()
 		{
-			var sql = @"SELECT sca_cli_for as CliId, cli_desc as CliDesc, cli_tel as CliTel,  SUM(sca_importo) as CliTotale
+			if (first)
+			{
+				var sql = @"SELECT sca_cli_for as CliId, cli_desc as CliDesc, cli_tel as CliTel,  SUM(sca_importo) as CliTotale
 				FROM scadenze 
 				INNER JOIN clienti1 on cli_codice = sca_cli_for
 				WHERE sca_relaz = 0 AND sca_pagato = 0
 				GROUP BY cli_desc, cli_tel, sca_cli_for";
 
-			busyIndicator.IsBusy = true;
-			dataGrid.IsBusy = true;
-			var scaList = await dbcon_.QueryAsync<ScadenzeInfo>(sql);
-			var scaCollection= new ObservableCollection<ScadenzeInfo>(scaList);
-			dataGrid.ItemsSource = scaCollection;
-			dataGrid.IsBusy = false;
-			busyIndicator.IsBusy = false;
+				busyIndicator.IsBusy = true;
+				dataGrid.IsBusy = true;
+				var scaList = await dbcon_.QueryAsync<ScadenzeInfo>(sql);
+				var scaCollection = new ObservableCollection<ScadenzeInfo>(scaList);
+				dataGrid.ItemsSource = scaCollection;
+				dataGrid.IsBusy = false;
+				busyIndicator.IsBusy = false;
+				first = false;
+			}
 			base.OnAppearing();
 		}
 
@@ -96,12 +113,40 @@ namespace Facile
 
 		void OnSwipeStarted(object sender, Syncfusion.SfDataGrid.XForms.SwipeStartedEventArgs e)
 		{
-			throw new NotImplementedException();
+			swipeIndex_ = e.RowIndex;
+			swipeData_ = (ScadenzeInfo)e.RowData;
 		}
 
 		void OnLeftBindingContextChanged(object sender, System.EventArgs e)
 		{
-			throw new NotImplementedException();
+			if (leftImage == null)
+			{
+				leftImage = sender as Image;
+				(leftImage.Parent as View).GestureRecognizers.Add(new TapGestureRecognizer() { Command = new Command(ShowDetails) });
+			}
 		}
+
+		private async void ShowDetails()
+		{
+			dataGrid.ResetSwipeOffset();
+			if (swipeIndex_ == 0) return; 
+			if (swipeData_ == null) return;
+
+			try
+			{
+				string sql = String.Format("SELECT * FROM scadenze WHERE sca_relaz = 0 AND sca_pagato = 0 AND sca_cli_for = {0}", swipeData_.CliId);
+				var scaList = await dbcon_.QueryAsync<Scadenze>(sql);
+			}
+			catch (Exception ex)
+			{
+				await DisplayAlert("Attenzione!", ex.Message, "OK");
+				return;
+			}
+
+			var page = new ScadenzeDetails(swipeData_);
+			await Navigation.PushAsync(page);
+		}
+
+
 	}
 }
