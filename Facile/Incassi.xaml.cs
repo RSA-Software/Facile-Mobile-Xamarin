717a6a65480a3a049f1e471d475285abfee07e14
Capizzi Filippo <capizzi@rsaweb.com>
2018-05-12 12:14:18 +0200

    Lavori su maschera incassi

--- Facile/Incassi.xaml.cs
+++ Facile/Incassi.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using Facile.Interfaces;
 using Facile.Models;
 using SQLite;
@@ -16,6 +17,8 @@ namespace Facile
 	{
 		private Clienti _cli;
 		private Destinazioni _dst;
+		private bool _cli_changed;
+		private bool _dst_changed;
 		private readonly SQLiteAsyncConnection _dbcon;
 
 		public Incassi(Clienti cli, Destinazioni dst)
@@ -51,6 +54,9 @@ namespace Facile
 				SummaryType = SummaryType.CountAggregate
 			});
 			dataGrid.TableSummaryRows.Add(summaryRow1);
+
+			_cli_changed = false;
+			_dst_changed = false;
 		}
 
 		protected async override void OnAppearing()
@@ -72,7 +78,17 @@ namespace Facile
 			base.OnAppearing();
 		}
 
-		async void OnClientiTapped(object sender, System.EventArgs e)
+		void OnClientiTextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+		{
+			_cli_changed = true;
+		}
+
+		void OnDestinazioniTextChanged(object sender, Xamarin.Forms.TextChangedEventArgs e)
+		{
+			_dst_changed = true;
+		}
+
+		async void OnClientiClicked(object sender, System.EventArgs e)
 		{
 			var page = new ClientiSearch();
 			page.CliList.ItemDoubleTapped += async (source, args) =>
@@ -80,7 +96,7 @@ namespace Facile
 				var cli = (Clienti)args.ItemData;
 				m_cli_cod.Text = cli.cli_codice.ToString();
 				m_cli_des.Text = cli.cli_desc;
-				await  Navigation.PopAsync();
+				await Navigation.PopAsync();
 
 				if ((_cli == null) || (_cli.cli_codice != cli.cli_codice))
 				{
@@ -95,13 +111,151 @@ namespace Facile
 					dataGrid.ItemsSource = scaCollection;
 					dataGrid.IsBusy = false;
 				}
+				_cli_changed = false;
+				_dst_changed = false;
 			};
 			await Navigation.PushAsync(page);
 		}
 
-		void Handle_Clicked(object sender, System.EventArgs e)
+		async void OnDestinazioniClicked(object sender, System.EventArgs e)
 		{
-			throw new NotImplementedException();
+			if ((_cli == null) || (_cli.cli_codice == 0)) return;
+
+			var recTotal_ = 0;
+			string sql = "SELECT COUNT(*) FROM destina1 WHERE dst_rel = 0 AND dst_cli_for = " + _cli.cli_codice;
+			recTotal_ = await _dbcon.ExecuteScalarAsync<int>(sql);
+			if (recTotal_ == 0)
+			{
+				await DisplayAlert("Attenzione!", "Non ci sono destinazioni per il Cliente selezionato", "OK");
+				return;
+			}
+			var page = new DestinazioniSearch(_cli != null ? _cli.cli_codice : 0);
+			page.DstList.ItemDoubleTapped += async (source, args) =>
+			{
+				var dst = (Destinazioni)args.ItemData;
+				m_dst_cod.Text = dst.dst_codice.ToString();
+				m_dst_des.Text = dst.dst_desc;
+				await Navigation.PopAsync();
+				if ((_dst == null) || (_dst.dst_codice != dst.dst_codice))
+				{
+					_dst = dst;
+					dataGrid.IsBusy = true;
+					sql = String.Format("SELECT * FROM scadenze WHERE sca_relaz = 0 AND sca_pagato = 0 AND sca_cli_for = {0} AND sca_dst = {1} ORDER BY sca_data, sca_num", _cli.cli_codice, _dst.dst_codice);
+					var scaList = await _dbcon.QueryAsync<Scadenze>(sql);
+					var scaCollection = new ObservableCollection<Scadenze>(scaList);
+					dataGrid.ItemsSource = scaCollection;
+					dataGrid.IsBusy = false;
+				}
+				_cli_changed = false;
+				_dst_changed = false;
+			};
+			await Navigation.PushAsync(page);
+		}
+
+		async void OnClientiUnfocused(object sender, Xamarin.Forms.FocusEventArgs e)
+		{
+			if (!_cli_changed) return;
+			try
+			{
+				_cli = await _dbcon.GetAsync<Clienti>(Convert.ToInt32(m_cli_cod.Text));
+			}
+			catch (Exception ex)
+			{
+				_cli = null;
+				Debug.WriteLine(ex.Message);
+			}
+			if (_dst != null)
+			{
+				if ((_cli == null) || (_dst.dst_cli_for != _cli.cli_codice) || (_dst.dst_rel != 0)) _dst = null;
+			}
+
+			m_cli_cod.Text = _cli != null ? _cli.cli_codice.ToString() : "";
+			m_cli_des.Text = _cli != null ? _cli.cli_desc : "";
+
+			m_dst_cod.Text = _dst != null ? _dst.dst_codice.ToString() : "";
+			m_dst_des.Text = _dst != null ? _dst.dst_desc : "";
+
+			List<Scadenze> scaList = new List<Scadenze>();
+			dataGrid.IsBusy = true;
+			if (_cli != null)
+			{
+				string sql = "";
+				if (_dst == null)
+					sql = String.Format("SELECT * FROM scadenze WHERE sca_relaz = 0 AND sca_pagato = 0 AND sca_cli_for = {0} ORDER BY sca_data, sca_num", _cli.cli_codice);
+				else
+					sql = String.Format("SELECT * FROM scadenze WHERE sca_relaz = 0 AND sca_pagato = 0 AND sca_cli_for = {0} AND sca_dst = {1} ORDER BY sca_data, sca_num", _cli.cli_codice, _dst.dst_codice);
+				scaList = await _dbcon.QueryAsync<Scadenze>(sql);
+			}
+			var scaCollection = new ObservableCollection<Scadenze>(scaList);
+			dataGrid.ItemsSource = scaCollection;
+			dataGrid.IsBusy = false;
+		}
+
+		async void OnDestinazioniUnfocused(object sender, Xamarin.Forms.FocusEventArgs e)
+		{
+			if (!_dst_changed) return;
+			try
+			{
+				if (_cli != null)
+				{
+					_dst = await _dbcon.GetAsync<Destinazioni>(Convert.ToInt32(m_dst_cod.Text));
+					if (_dst.dst_cli_for != _cli.cli_codice || _dst.dst_rel != 0) _dst = null;
+				}
+				else _dst = null;
+			}
+			catch (Exception ex)
+			{
+				_dst = null;
+				Debug.WriteLine(ex.Message);
+			}
+			m_cli_cod.Text = _cli != null ? _cli.cli_codice.ToString() : "";
+			m_cli_des.Text = _cli != null ? _cli.cli_desc : "";
+
+			m_dst_cod.Text = _dst != null ? _dst.dst_codice.ToString() : "";
+			m_dst_des.Text = _dst != null ? _dst.dst_desc : "";
+
+			List<Scadenze> scaList = new List<Scadenze>();
+			dataGrid.IsBusy = true;
+			if (_cli != null)
+			{
+				string sql = "";
+				if (_dst == null)
+					sql = String.Format("SELECT * FROM scadenze WHERE sca_relaz = 0 AND sca_pagato = 0 AND sca_cli_for = {0} ORDER BY sca_data, sca_num", _cli.cli_codice);
+				else
+					sql = String.Format("SELECT * FROM scadenze WHERE sca_relaz = 0 AND sca_pagato = 0 AND sca_cli_for = {0} AND sca_dst = {1} ORDER BY sca_data, sca_num", _cli.cli_codice, _dst.dst_codice);
+				scaList = await _dbcon.QueryAsync<Scadenze>(sql);
+			}
+			var scaCollection = new ObservableCollection<Scadenze>(scaList);
+			dataGrid.ItemsSource = scaCollection;
+			dataGrid.IsBusy = false;
+		}
+
+		void OnSalvaClicked(object sender, System.EventArgs e)
+		{
+			/*
+			try
+			{
+				var message = new MailMessage
+				{
+					Subject = "Incasso",
+					Body = "Ho incassato 100.000,00 €",
+					To = "mariorifici@gmail.com"
+					//Cc = ccRecipients,
+					//Bcc = bccRecipients
+				};
+
+				await Email.ComposeAsync(message);
+			}
+			catch (FeatureNotSupportedException fbsEx)
+			{
+				// Sms is not supported on this device
+			}
+			catch (Exception ex)
+			{
+				// Some other exception occured
+			}
+*/
 		}
 	}
 }
+
