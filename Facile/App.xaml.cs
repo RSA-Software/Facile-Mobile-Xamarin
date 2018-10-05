49d2e928d6a77da942fdcc727bbeb7c22b6a0fa9
Capizzi Filippo <capizzi@rsaweb.com>
2018-10-05 09:58:35 +0200

    * Facile.Droid.csproj: Aggiornamento componenti syncfusion e   applicazioni modifiche per corretto editing griglia incassi secondo   specifiche componente aggiornato.
    * App.xaml.cs:
    * Incassi.xaml:
    * packages.config:
    * Facile.csproj:
    * packages.config:
    * Facile.iOS.csproj:
    * packages.config:
    * Resource.designer.cs:

--- Facile/App.xaml.cs
+++ Facile/App.xaml.cs
@@ -24,10 +24,10 @@ namespace Facile
 			facile_db_impo = null;
 
 			// 16.2.x.x
-			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzUyQDMxMzYyZTMyMmUzMEZpOGZMSHMvU3hFMU5Dazc2QVNJYjlPdWhMWlZOSHhFRjFLZ1RNbE55RTg9");
+			//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzUyQDMxMzYyZTMyMmUzMEZpOGZMSHMvU3hFMU5Dazc2QVNJYjlPdWhMWlZOSHhFRjFLZ1RNbE55RTg9");
 
 			// 16.3.x.x
-			//Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjg3MTVAMzEzNjJlMzMyZTMwWjJMQ1RRTlFXNkdnaWpzS0l6MGcyTFlWQzhJSnZvK01mNyt0Y2hiTzZzVT0=");
+			Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mjg3MTVAMzEzNjJlMzMyZTMwWjJMQ1RRTlFXNkdnaWpzS0l6MGcyTFlWQzhJSnZvK01mNyt0Y2hiTzZzVT0=");
 
 			InitializeComponent();
 			MainPage = new NavigationPage(new FacilePage());
